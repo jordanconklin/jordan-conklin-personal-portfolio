b1df5902de0fb8d8382ccedd89e198406cb87a4c
Jordan Conklin <jordinho@ucla.edu>
2024-02-15 03:40:08 -0800

    no more arrow

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -40,7 +40,7 @@ const HeroSection = () => {
           </Link>
         </div>
       </div>
-      <div className="flex flex-row items-center text-center justify-center ">
+      {/* <div className="flex flex-row items-center text-center justify-center ">
         <Link
           to="about"
           activeClass="active"
@@ -51,7 +51,7 @@ const HeroSection = () => {
         >
             <HiArrowDown size={35} className="animate-bounce" />
         </Link>
-      </div>
+      </div> */}
     </section>
   )
 }
